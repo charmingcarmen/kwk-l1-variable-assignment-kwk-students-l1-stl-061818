1a7664a0d7bc8b4e3929d8e02ef373e669498d96
charmingcarmen <bradleyc755@ritenourschools.org>
2018-06-18 19:43:31 +0000

    automatically backed up by learn

--- variable.rb
+++ variable.rb
@@ -1,2 +1,13 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+# a = 7
+# a + 2 
+# puts a + b 
+#assigning variables
+# when assigning variable use lowercase letters 
+# name - carmen
+# puts name 
+name= "carmen" 
+last_name ="bradley" 
+age = 15
+puts "my name is " #{name} # {last_name} and i am  #{age}
